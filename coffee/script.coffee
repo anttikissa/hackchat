61286e5610d772946ec4b633a49bd1f61f7208d0
Antti Sykäri <antti.sykari@gmail.com>
2012-11-10 12:07:07 +0200

    Measure roundtrip time.

--- coffee/script.coffee
+++ coffee/script.coffee
@@ -1,9 +1,19 @@
+ping = ->
+	socket.emit('ping', { ts: new Date().getTime() })
+
 $ ->
 	$('body').append('<p>Hello from coffee</p>')
+	$('button').click ->
+		ping()
+
 	socket.on 'connect', ->
-		socket.emit('ping', { ts: new Date().getTime() })
+		ping()
+
 	socket.on 'pong', (data) ->
-		$('body').append("<p>PONG #{JSON.stringify data}")
+		backThen = data.ts
+		now = new Date().getTime()
+
+		$('body').append("<p>PONG #{JSON.stringify data}, roundtrip #{now - backThen} ms")
 
 socket = io.connect()
 
--- public/script.js
+++ public/script.js
@@ -1,14 +1,24 @@
-var socket;
+var ping, socket;
+
+ping = function() {
+  return socket.emit('ping', {
+    ts: new Date().getTime()
+  });
+};
 
 $(function() {
   $('body').append('<p>Hello from coffee</p>');
+  $('button').click(function() {
+    return ping();
+  });
   socket.on('connect', function() {
-    return socket.emit('ping', {
-      ts: new Date().getTime()
-    });
+    return ping();
   });
   return socket.on('pong', function(data) {
-    return $('body').append("<p>PONG " + (JSON.stringify(data)));
+    var backThen, now;
+    backThen = data.ts;
+    now = new Date().getTime();
+    return $('body').append("<p>PONG " + (JSON.stringify(data)) + ", roundtrip " + (now - backThen) + " ms");
   });
 });
 
