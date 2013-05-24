707196f2d0ebe8a4d49730e3f07973f0f1d5655f
Antti Sykäri <antti.sykari@gmail.com>
2013-05-24 14:01:28 +0300

    Tune deploy script.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,5 +10,6 @@ cd $(dirname $0)/..
 ssh $TARGETHOST mkdir -p /opt/apps/$NAME
 rsync -r --exclude .git --exclude-from=.gitignore . $TARGETHOST:/opt/apps/$NAME
 ssh $TARGETHOST sudo cp /opt/apps/$NAME/deploy/$NAME.conf /etc/init/
+ssh $TARGETHOST "cd /opt/apps/$NAME && npm install"
 ssh $TARGETHOST "sudo stop $NAME; sudo start $NAME"
 
